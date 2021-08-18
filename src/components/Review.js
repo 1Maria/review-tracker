ae7ad8753fc1f2ee11ff152e853f0f34d4b248de
full_name <email_address>
2021-08-17 20:51:23 -0400

    Create truncation for index view and adjust spacing on review card

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { format } from 'date-fns'
 import { Box, Typography, makeStyles } from '@material-ui/core';
+import clsx from 'clsx';
 import StarIcon from '@material-ui/icons/Star';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import ForumIcon from '@material-ui/icons/Forum';
@@ -26,32 +27,47 @@ const useStyles = makeStyles((theme) => ({
     content: {
         color: theme.palette.secondary.darkGray,
         paddingTop: '1.5625rem',
-        paddingBottom: '6.0625rem', 
+        height: '8.25rem',
     }, 
     author: {
         color: theme.palette.secondary.main, 
-        paddingBottom: '1.5625rem',
+        paddingBottom: '1.25rem',
     },
     date: {
         color: theme.palette.secondary.lightGray,
-        paddingBottom: '1.5625rem',
+        paddingBottom: '1.25rem',
+    }, 
+    bottomLine: {
+        display: 'flex', 
+        alignItems: 'space-between', 
+        justifyContent: 'space-between',
+    },
+    bottomLineLarge: {
+        marginTop: '3rem',
     }
 }));
 
 const Review = ({review, isSmall}) => {
     const classes = useStyles();
+    let contentTruncation = review.content;
+    if (isSmall && review.content.length > 80) {
+        const substring = review.content.substring(0, 80);
+        const closestSpace = substring.split('').findIndex((char) => char === ' ') + 80;
+        contentTruncation = review.content.substring(0, closestSpace) + '...';
+    }
+
     return (
-        <Box className={`${classes.review} ${isSmall && classes.smallReview}`}>
+        <Box className={clsx(classes.review, isSmall && classes.smallReview)}>
             <Typography component="h2" variant="h2" className={classes.place}>
                 {review.place}
             </Typography>
             {new Array(review.rating).fill(0).map(rating => <StarIcon style={{fill: '#FBCD33'}}/>)}
             {new Array(5 - review.rating).fill(0).map(rating => <StarBorderIcon style={{fill: '#FBCD33'}}/>)}
             <Typography component="h3" variant="h3" className={classes.content}>
-                {review.content}
+                {contentTruncation}
             </Typography>
-            <Box style={{ display: 'flex', alignItems: 'space-between', justifyContent: 'space-between'}}>
-                <Box style={{ display: 'flex', alignItems: 'space-between', justifyContent: 'space-between', gap: '1.75rem'}}>
+            <Box className={clsx(classes.bottomLine, !isSmall && classes.bottomLineLarge)} >
+                <Box className={classes.bottomLine} style={{gap: '1.75rem'}}>
                     <Typography component="h4" variant="h4" className={classes.author}>
                         {review.author}
                     </Typography>
